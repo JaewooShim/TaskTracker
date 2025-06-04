01e4b21b70abcc5ad980a40f4ff8fa0fe9a5543f
Jaewoo-shim-UTCS <tlawodn3656@gmai.com>
2025-06-04 14:25:36 +0900

    store authroized client in db instead of inmemory

---
+++ tasks/src/main/java/com/tasktrack/tasks/domain/auth/service/CustomOAuth2ClientService.java
@@ -0,0 +1,19 @@
+package com.tasktrack.tasks.domain.auth.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+@Configuration
+public class CustomOAuth2ClientService {
+
+    @Bean
+    public OAuth2AuthorizedClientService oAuth2AuthorizedClientService(JdbcTemplate jdbcTemplate,
+                                                                       ClientRegistrationRepository
+                                                                               clientRegistrationRepository) {
+        return new JdbcOAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepository);
+    }
+}
